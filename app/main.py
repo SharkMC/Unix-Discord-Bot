46f9be7c57ef894faca9f08ca1169f95aa1bd0c4
ShqrkMC <160005502+SharkMC@users.noreply.github.com>
2025-02-05 07:33:41 +0900

    main.py の更新

--- app/main.py
+++ app/main.py
@@ -1,1 +1,43 @@
-あ
+import discord
+import os
+import requests
+
+form server server_thread
+
+dotenv.load_dotenv()
+
+TOKEN = os.environ.get("TOKEN")
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'ログインしました: {self.user}')
+
+    async def on_message(self, message):
+        print(f'送信: {message.author}: {message.content}')
+        if message.author == self.user:
+            return
+
+        if message.content == '!info':
+            await message.channel.send('Discord: https://discord.gg/zns7VZteC2')
+
+        elif message.content == '!staff':
+            await message.channel.send('-------« Staff Member »-------\nShqrkMC - Founder, Owner, Dev\neozah - Owner, Dev\nUran3007 - Manager, Builder\nx Ramuneee - Admin, Builder\nYuk1yQwQ - Admin\n--------------------------------')
+
+        elif message.content == '!status':
+            try:
+                url = f"https://api.mcsrvstat.us/bedrock/2/unix.f5.si:25720"
+
+                responce = requests.get(url)
+                data = responce.json()
+                if data['online']:
+                    await message.channel.send(f"{data['players']['online']} / 100")
+                else:
+                    await message.channel.send("The server is offline")
+            except Exception as e:
+                    await message.channel.send(f"Error: {e}")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+server_thread()
+client.run(TOKEN)
